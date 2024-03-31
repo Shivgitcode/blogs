d22e1984b2e4f79a64b3b8447d5c427868f8dfa1
shivansh <shivneeraj2004@gmail.com>
2024-03-31 11:51:51 +0530

    blog project almost done

--- src/components/Blogs.tsx
+++ src/components/Blogs.tsx
@@ -1,17 +1,18 @@
+import { Link } from "react-router-dom";
 import { blogs } from "../constants";
 
 export default function Blogs({ posts }: { posts: any }) {
   return (
     <div className="flex w-[70%]  justify-start gap-[70px] items-center flex-wrap  mx-auto ">
-      {posts.map((el: any) => {
+      {posts?.map((el: any) => {
         return (
-          <div className="flex flex-col ">
+          <div className="flex flex-col " key={el.id}>
             <div className="w-[400px]">
               <img src={el.img} alt="not found" className="w-full" />
             </div>
             <div>
               <div className=" mt-2 font-bold text-[20px]">
-                <h3>{el.title}</h3>
+                <Link to={`/blog/${el.id}`}>{el.title}</Link>
               </div>
               <div className="hidden">
                 <p>{el.description}</p>
--- src/pages/OneBlog.tsx
+++ src/pages/OneBlog.tsx
@@ -1,6 +1,63 @@
 import { useParams } from "react-router-dom";
+import { blogs } from "../constants";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 
 export default function OneBlog() {
   const { id } = useParams();
-  return <div>One {id}</div>;
+  const [userId, setUserId] = useState("");
+  const [oneUser, setOneUser] = useState({
+    title: "",
+    img: "",
+    description: "",
+  });
+  // const oneBlog = blogs[0];
+  useEffect(() => {
+    setUserId(id as string);
+    const fetchData = async () => {
+      const response = await fetch(
+        `http://localhost:4000/api/v1/post/${userId}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          mode: "cors",
+          credentials: "include",
+        }
+      );
+      if (response.ok) {
+        const res_data = await response.json();
+        const { data } = res_data;
+        console.log(res_data);
+        setOneUser(data);
+        toast.success("data fetched successfully");
+      } else {
+        const res_data = await response.json();
+        console.log(res_data);
+        toast.error(res_data.error);
+      }
+    };
+    fetchData();
+  }, [userId]);
+
+  return (
+    <div className="w-[70%] mx-auto h-full">
+      <div className="flex flex-col items-start gap-6 mt-10">
+        <h1 className="font-bold text-[42px]">{oneUser.title}</h1>
+        <div className="w-full">
+          <div className="w-[75%] mx-auto">
+            <img
+              src={oneUser.img}
+              className="w-full mx-auto rounded-lg"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="font-normal mt-10 text-[23px] leading-8">
+          <p>{oneUser.description}</p>
+        </div>
+      </div>
+    </div>
+  );
 }
